7c61a68d98852f4d0529720c1965095b330e4943
ua-showt <alekseev.rv@gmail.com>
2020-08-25 18:19:34 +0300

    test commit

--- test.js
+++ test.js
@@ -1,68 +1,18 @@
-{
-    "id": "ssoFormLinksMain",
-    "type": "Scene",
-    "title": "Связи клиента",
-    "subtitle": "Получение ссылки на рабочий стол клиента из связи",
-    "children": [{
-        "id": "ssoFormLinks",
-        "type": "Form",
-        "onSubmit": {
-            "action": "https://msb-nodered-test.amazon.loc/bots/ssoform/desktop/step1/",
-            "defaultModel": {
-                "list": [{
-                    "val": "МІТЯГІНА НАТАЛІЯ ОЛЕКСІЇВНА | РОДИТЕЛЬСКАЯ | MAIN",
-                    "key": "49793947"
-                }, {
-                    "val": "КОЛОМОЙЦЕВ АНТОН ІГОРОВИЧ | РОДИТЕЛЬСКАЯ | MAIN",
-                    "key": "1000263483"
-                }]
-            }
-        },
-        "children": [{
-            "id": "text1",
-            "type": "TextView",
-            "value": "Рабочий стол клиента из диалога",
-            "marginBottom": "xSmall"
-        }, {
-            "id": "desktop",
-            "type": "Button",
-            "value": "Основной клиент",
-            "mode": "raised",
-            "skin": "regular",
-            "onClick": [{
-                "type": "link",
-                "url": "https://inc-start.privatbank.ua/DesktopGUI/callcentredesktop.html?clid=15843647&phone=%2B380503105552"
-            }]
-        }, {
-            "id": "separator",
-            "type": "Separator"
-        }, {
-            "id": "text2",
-            "type": "TextView",
-            "value": "Для получения доступа к рабочему столу к клиенту из связей, выберите его из списка и нажмите 'Получить'.\nMAIN-основной в связи\nSUB-подчиненный в связи",
-            "valueSize": "xSmall",
-            "marginBottom": "xSmall"
-        }, {
-            "id": "choice",
-            "type": "List",
-            "label": "Клиенты из связей:",
-            "value": "49793947",
-            "list": [{
-                "val": "МІТЯГІНА НАТАЛІЯ ОЛЕКСІЇВНА | РОДИТЕЛЬСКАЯ | MAIN",
-                "key": "49793947"
-            }, {
-                "val": "КОЛОМОЙЦЕВ АНТОН ІГОРОВИЧ | РОДИТЕЛЬСКАЯ | MAIN",
-                "key": "1000263483"
-            }]
-        }, {
-            "type": "Div",
-            "direction": "row",
-            "id": "divBt",
-            "children": [{
-                "id": "submit",
-                "type": "Button",
-                "value": "Получить"
-            }]
-        }]
-    }]
-}
+msg.payload = {
+    "data": {
+        "companyId": msg.data.companyId,
+        "view": view,
+        "channelId": msg.data.channelId,
+        "baseMsgId": msg.data.baseMsgId,
+        "viewModel": {
+            text2: text2,
+            vis: vis,
+            btAddVis: btAddVis,
+            ds: "step1"
+        }
+    },
+    "action": "msgView"
+};
+msg.headers = {
+    "X-Auth-Token": global.get(msg.data.companyId)
+};
---
+++ test3.js
@@ -0,0 +1,42 @@
+{
+    "a.arrestResponse": {
+        "reqMRef": "chat_pafy5_1598014909020",
+        "reqMId": "CHAT",
+        "reqBank": "PB",
+        "wfAccountArrestList": {
+            "wfAccountArrestList": [{
+                "arrestId": 5455,
+                "type": "E",
+                "refContract": "SAMDNWFC00005888667",
+                "ekbId": 1000974740,
+                "dateStart": "2020-08-13T09:51:54+03:00",
+                "state": "a",
+                "amount": 2120,
+                "currency": 980,
+                "amountEquivalent": 2120,
+                "currencyEquivalent": 980,
+                "reason": "VK 200810PBNKI000000003662",
+                "isCashlim": "Y",
+                "panList": {
+                    "panList": ["4149497833372719", "4149497843117922", "4149497810188013", "5168757388863450"]
+                }
+            }, {
+                "arrestId": 5454,
+                "type": "E",
+                "refContract": "SAMDNWFC00005888667",
+                "ekbId": 1000974740,
+                "dateStart": "2020-08-13T09:50:41+03:00",
+                "state": "a",
+                "amount": 672.62,
+                "currency": 980,
+                "amountEquivalent": 672.62,
+                "currencyEquivalent": 980,
+                "reason": "VK 200813PBNKI000000001080",
+                "isCashlim": "Y",
+                "panList": {
+                    "panList": ["4149497833372719", "4149497843117922", "4149497810188013", "5168757388863450"]
+                }
+            }]
+        }
+    }
+}
