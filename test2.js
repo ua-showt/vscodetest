ffe6d6e470160a5d21e009ded0aaf6a882851cdb
ua-showt <alekseev.rv@gmail.com>
2020-08-31 10:56:04 +0300

    test pluggins

--- test2.js
+++ test2.js
@@ -1,1 +1,37 @@
-console.log(true)
+{
+    "r": [{
+        "BLN_BYCLIENTID": [{
+            "BLN_BYCLIENTID": {
+                "TYPES": [{
+                    "BlackType2": 1,
+                    "BlackType1": 1
+                }],
+                "EditorLogin": "BLACKLISTS",
+                "BlackTable": "C",
+                "BlackId": 6503127,
+                "CtoBDTM": "2013-01-23 07:51:16.32",
+                "BlackCountry": "UA",
+                "SrcID": 2,
+                "CtoBCode": "B",
+                "CtoBRemark": "REDEFINE",
+                "CtoBSt": "A"
+            }
+        }, {
+            "BLN_BYCLIENTID": {
+                "TYPES": [{
+                    "BlackType2": 1,
+                    "BlackType1": 1
+                }],
+                "EditorLogin": "BLACKLISTS",
+                "BlackTable": "C",
+                "BlackId": 6503127,
+                "CtoBDTM": "2013-01-23 07:51:16.32",
+                "BlackCountry": "UA",
+                "SrcID": 2,
+                "CtoBCode": "D",
+                "CtoBRemark": "REDEFINE",
+                "CtoBSt": "A"
+            }
+        }]
+    }]
+}
